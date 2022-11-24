a725e8587bf633d21d5f68558ec9d0c8fa52a000
keres <keres.kgb@gmail.com>
2022-11-24 17:46:46 -0500

    add gui login

--- src/main/java/com/kata/main/Kata.java
+++ src/main/java/com/kata/main/Kata.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.kata.kata;
+package com.kata.main;
+
+import com.kata.main.GUI.Login;
 
 /**
  *
@@ -13,5 +15,7 @@ public class Kata {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Login login=new Login();
+        login.setVisible(true);
     }
 }
