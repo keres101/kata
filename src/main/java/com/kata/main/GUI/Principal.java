c436739095633b64dc57e8c739702364050023d9
keres <keres.kgb@gmail.com>
2022-11-26 20:02:35 -0500

    socket add, sens message and listener message

--- src/main/java/com/kata/main/GUI/Principal.java
+++ src/main/java/com/kata/main/GUI/Principal.java
@@ -8,8 +8,12 @@ import javax.swing.JButton;
 import com.kata.main.Chat.Chat;
 import com.kata.main.Server;
 import com.kata.main.Usuario;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 /**
  *
  * @author ERIC
@@ -17,12 +21,27 @@ import java.awt.event.ActionListener;
 public class Principal extends javax.swing.JFrame {
 
     public Usuario usuario;
+    Socket socket;
+    public String chatId;
     
     public Principal(Usuario user) {
         initComponents();
         usuario = user;
         MostrarBotonesChats(usuario);
         
+        URI uri=URI.create("http://127.0.0.1:3000");
+        IO.Options options=IO.Options.builder().build();
+        socket=IO.socket(uri,options);  
+        socket.connect();
+        
+        socket.on("message",new Emitter.Listener(){
+            @Override
+            public void call(Object... args){
+                String message=args[0].toString();
+                String beforeText=txtChat.getText();
+                txtChat.setText(beforeText+message+"\n");
+            }
+        });
     }
 
     @SuppressWarnings("unchecked")
@@ -32,7 +51,7 @@ public class Principal extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         txtChat = new javax.swing.JTextArea();
         jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        inputMessage = new javax.swing.JTextField();
         panelBotones = new javax.swing.JPanel();
         AddFriend = new javax.swing.JButton();
 
@@ -43,6 +62,16 @@ public class Principal extends javax.swing.JFrame {
         jScrollPane1.setViewportView(txtChat);
 
         jButton1.setText("Enviar");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         panelBotones.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
@@ -77,7 +106,7 @@ public class Principal extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 364, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jTextField1)
+                        .addComponent(inputMessage)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(16, 16, 16))
@@ -92,7 +121,7 @@ public class Principal extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE)
-                            .addComponent(jTextField1)))
+                            .addComponent(inputMessage)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(32, 32, 32)
                         .addComponent(AddFriend, javax.swing.GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE)
@@ -110,6 +139,20 @@ public class Principal extends javax.swing.JFrame {
         add.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_AddFriendActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        // TODO add your handling code here:
+        System.out.println(inputMessage.getText());
+        String message=inputMessage.getText();
+        if(!message.equals("")){
+            socket.emit("message", usuario.getToken(),chatId,message);
+            inputMessage.setText("");
+        } 
+    }//GEN-LAST:event_jButton1MouseClicked
 
     
     public void MostrarBotonesChats(Usuario user){
@@ -159,7 +202,7 @@ public class Principal extends javax.swing.JFrame {
     public void ObtenerIdChat(ActionEvent e)
     {
         System.out.println("Consultando chatId : " + e.getActionCommand());
-        
+        chatId=e.getActionCommand();
         Chat chat=Server.obtenerMensajes(e.getActionCommand(), usuario.getToken());
         if(chat.getMessages() !=null && chat.getMessages().length >0){
             for(int i=0;i<chat.getMessages().length;i++){
@@ -168,15 +211,18 @@ public class Principal extends javax.swing.JFrame {
                 txtChat.setText(textBefore+newMessage+"\n");
             }
         }
+        if(chat.getMessages()==null){
+            txtChat.setText("");
+        }
         
         
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton AddFriend;
+    private javax.swing.JTextField inputMessage;
     private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JPanel panelBotones;
     private javax.swing.JTextArea txtChat;
     // End of variables declaration//GEN-END:variables
